b1e143784e3fed9605afd4bfe5da572c1b982267
6Gsus9 <jesusandrescamargo0@gmail.com>
2021-07-04 17:16:16 -0500

    prueba

--- src/HolaQuien.java
+++ src/HolaQuien.java
@@ -5,7 +5,7 @@ public class HolaQuien {
         var sac = new Scanner(System.in);
         System.out.println("Por favor ingrese su nombre");
         var nombre = sac.nextLine();
-        System.out.println("Hola " + nombre + "!!!");
+        System.out.println("Hola " + nombre + "!");
 
     }
     
